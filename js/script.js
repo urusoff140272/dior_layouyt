7e92ba917bdda7bde521cc28952b8c8bf01a15e2
urusoff140272 <107813230+urusoff140272@users.noreply.github.com>
2024-03-30 12:06:44 +0200

    feat: add html, css and js setions top and about

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-"use strict"
+const swiperTop = new Swiper('.top__swiper', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+const swiperAbout = new Swiper(".about__swiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    freeMode: true,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+});
